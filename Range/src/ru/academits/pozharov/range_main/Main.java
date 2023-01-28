364ba5352efacd1d1f8b8c7b93c95b7734c800f9
Rpozharov <rom.pozharov@gmail.com>
2023-01-28 12:24:15 +0700

    Edit Range project

--- Range/src/ru/academits/pozharov/range_main/Main.java
+++ Range/src/ru/academits/pozharov/range_main/Main.java
@@ -1,7 +1,35 @@
 package ru.academits.pozharov.range_main;
 
+import ru.academits.pozharov.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Range range = new Range(0, 0);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите начальное число диапазона: ");
+        double startNumber = scanner.nextDouble();
+
+        System.out.print("Введите конечное число диапазона: ");
+        double endNumber = scanner.nextDouble();
+
+        range.setFrom(startNumber);
+        range.setTo(endNumber);
+
+        System.out.println("Начальное число диапазона = " + range.getFrom());
+        System.out.println("Конечное число диапазона = " + range.getTo());
+        System.out.println("Длина диапазона = " + range.getLength());
+
+        System.out.print("Введите число для проверки, принадлежит ли число диапазону: ");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Число " + number + " принадлежит диапазону");
+        } else {
+            System.out.println("Число " + number + " не принадлежит диапазону");
+        }
     }
 }
