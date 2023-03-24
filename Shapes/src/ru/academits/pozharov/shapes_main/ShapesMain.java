36581df53994dc6a948c0b40f2ba86190539c9ed
Rpozharov <rom.pozharov@gmail.com>
2023-03-24 22:19:21 +0700

    Доработан класс Range, добавлен класс MainTwoRanges c main. Исправлены замечания в классах: Range, RangeMain,TwoRangeMain. Изменено название переменной endNumber2. Для печати intersectionRange использован переопределенный метод toString. Исправлена функция getDifference. Создана иерархия классов для геометрических фигур. Добавлены классы: Square, Triangle, Rectangle, Circle, интерфейс Shape. Добавлены два класса: SortByArea, SortByPerimeter для сортировки с компаратором. Добавлены геттеры для полей фигур. Компараторы вынесены в отдельный пакет, классы переименованы в figurePerimeterComparator, figureAreaComparator. Массив объявлен кратким синтаксисом. В классах изменены переопределённые методы toString.

--- Shapes/src/ru/academits/pozharov/shapes_main/ShapesMain.java
+++ Shapes/src/ru/academits/pozharov/shapes_main/ShapesMain.java
@@ -1,6 +1,7 @@
 package ru.academits.pozharov.shapes_main;
 
 import ru.academits.pozharov.shapes.*;
+import ru.academits.pozharov.shapes_comparators.figureAreaComparator;
 import ru.academits.pozharov.shapes_comparators.figurePerimeterComparator;
 
 import java.util.Arrays;
@@ -24,12 +25,15 @@ public class ShapesMain {
     }
 
     public static Shape getFigureWithMaxArea(Shape[] shapes) {
-        Arrays.sort(shapes, new figurePerimeterComparator());
-
         if (shapes.length == 0) {
             return null;
         }
 
+        if (shapes.length == 1) {
+            return shapes[0];
+        }
+
+        Arrays.sort(shapes, new figureAreaComparator());
         return shapes[shapes.length - 1];
     }
 
